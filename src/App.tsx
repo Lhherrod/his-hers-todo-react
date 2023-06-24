6335debf101ec7b619de6976bb609a311cca0ef9
Larry Herrod <lhherrod@gmail.com>
2023-06-24 10:12:11 -0500

    refactor on these employers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { db, auth, storage } from "./config/firebase";
 import { NewTaskType } from "./types/tasks";
+import { AddTask } from "./components/AddTask";
 import {
   getDocs,
   collection,
@@ -12,6 +13,7 @@ import {
 import Auth from "./components/Auth";
 import ListTable from "./components/ListTable";
 import { ref, uploadBytes } from "firebase/storage";
+import { signOut } from "firebase/auth";
 
 function App() {
   const [tasks, setTasks] = useState<NewTaskType[]>([]);
@@ -20,18 +22,27 @@ function App() {
   const handleHerSelectedTask = (task: NewTaskType) => {
     console.log(task);
   };
+
   const handleHisSelectedTask = (task: NewTaskType) => {
     console.log(task);
   };
+
   const handleDeleteTask = async (id: string) => {
     const taskDoc = doc(db, "tasks", id);
     await deleteDoc(taskDoc);
     getTasks();
   };
+
   const handleUpdateTask = async (id: string, task: string) => {
     const taskDoc = doc(db, "tasks", id);
     await updateDoc(taskDoc, { description: task });
     getTasks();
+  };
+
+  const handleIsLoggedIn = () => {
+    console.log(isLoggedIn);
+    isLoggedIn ? setIsLoggedIn(false) : setIsLoggedIn(true);
+    console.log(isLoggedIn);
   };
 
   const getTasks = async () => {
@@ -47,7 +58,11 @@ function App() {
     }
   };
 
-  const onSubmit = async () => {
+  const handleNewTaskWhoseTask = async (
+    newTaskDescription: string,
+    newTaskWhoseTask: string,
+    newTaskComplete: boolean
+  ) => {
     try {
       await addDoc(tasksCollection, {
         description: newTaskDescription,
@@ -75,65 +90,56 @@ function App() {
     }
   };
 
-  const [newTaskDescription, setNewTaskDescription] = useState("");
-  const [newTaskWhoseTask, setNewTaskWhoseTask] = useState("");
-  const [newTaskComplete, setNewTaskComplete] = useState(false);
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      handleIsLoggedIn();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [fileUpload, setFileUpload] = useState<File | null>(null);
 
   useEffect(() => {
     getTasks();
+
+    // persist loggedIn user hack until I get busy with Redux or some store
+    setTimeout(() => {
+      auth?.currentUser?.email && setIsLoggedIn(true);
+    }, 200);
   }, []);
 
   return (
-    <div>
-      <Auth />
-      <div>
-        <label htmlFor="description">What Task Is This?</label>
-        <input
-          type="text"
-          id="description"
-          onChange={(e) => setNewTaskDescription(e.target.value)}
-        />
-
-        <label htmlFor="whoseTask">Whose Task is it?</label>
-        <select
-          name="whoseTask"
-          id="whoseTask"
-          defaultValue={"-"}
-          onChange={(e) => setNewTaskWhoseTask(e.target.value)}
-        >
-          <option disabled>-</option>
-          <option value="hers">hers</option>
-          <option value="his">his</option>
-        </select>
-
-        <label htmlFor="complete">Is Task Completed?</label>
-        <input
-          type="checkbox"
-          id="complete"
-          checked={newTaskComplete}
-          onChange={(e) => setNewTaskComplete(e.target.checked)}
-        />
-
-        <button onClick={onSubmit}>Add Task</button>
-      </div>
-      <ListTable
-        tasks={tasks}
-        onSelectHerTask={handleHerSelectedTask}
-        onSelectHisTask={handleHisSelectedTask}
-        onDeleteTask={handleDeleteTask}
-        onUpdateTask={handleUpdateTask}
-      />
-
-      <div>
-        <label htmlFor="file-upload"></label>
-        <input
-          type="file"
-          id="file-upload"
-          onChange={(e) => setFileUpload(e.target.files?.[0] || null)}
-        />
-        <button onClick={uploadFile}>Upload File</button>
-      </div>
+    // will decide where to place this later
+    // <div>
+    //  <label htmlFor="file-upload"></label>
+    //  <input
+    //   type="file"
+    //   id="file-upload"
+    //   onChange={(e) => setFileUpload(e.target.files?.[0] || null)}
+    //   />
+    //  <button onClick={uploadFile}>Upload File</button>
+    // </div>
+    <div className="container lg:w-2/3 mx-auto py-5">
+      {!isLoggedIn ? (
+        <Auth isLoggedIn={handleIsLoggedIn} />
+      ) : (
+        <div>
+          <ListTable
+            tasks={tasks}
+            onSelectHerTask={handleHerSelectedTask}
+            onSelectHisTask={handleHisSelectedTask}
+            onDeleteTask={handleDeleteTask}
+            onUpdateTask={handleUpdateTask}
+          />
+          <AddTask onSetNewTaskWhoseTask={handleNewTaskWhoseTask} />
+          <button onClick={logOut} className="text-2xl text-indigo-700">
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/ListTable.tsx
+++ src/components/ListTable.tsx
@@ -30,7 +30,126 @@ function ListTable({
 
   return (
     <>
-      <div className="mx-auto w-2/5">
+      {herTasks.length && (
+        <div>
+          <h1 className="text-center text-purple-400 text-3xl md:text-5xl">
+            Her Tasks
+          </h1>
+          <div className="container mx-auto w-full tasks-center justify-center">
+            <ul className="flex flex-col bg-pink-300 p-4">
+              {herTasks.map((task, index) => (
+                <li
+                  className={
+                    herSelectedTask === index
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }
+                  onClick={() => {
+                    setHerSelectedTask(index);
+                    onSelectHerTask(task);
+                  }}
+                  key={index}
+                >
+                  <div className="select-none bg-gray-200 rounded-md flex flex-1 tasks-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg mb-4">
+                    <div className="flex flex-col rounded-md w-10 h-10 bg-gray-300 justify-center tasks-center mr-4">
+                      ⚽️
+                    </div>
+                    <div className="flex-1 pl-1 mr-16">
+                      <div className="font-medium">{task.description}</div>
+                      <div className="text-gray-600 text-sm">200ml</div>
+                    </div>
+                    <div className="text-gray-600 text-xs">
+                      6:00 AM
+                      <button
+                        className="text-red-500 ml-2"
+                        onClick={() => onDeleteTask(task.id!)}
+                      >
+                        delete task
+                      </button>
+                      <div>
+                        <label htmlFor="update-task">Update Task</label>
+                        <input
+                          id="update-task"
+                          className="text-indigo-500"
+                          onChange={(e) => setUpdatedTask(e.target.value)}
+                        />
+                        <button
+                          onClick={() => onUpdateTask(task.id!, updatedTask)}
+                        >
+                          Update Task
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <Button />
+          </div>
+        </div>
+      )}
+      {hisTasks.length && (
+        <div>
+          <h1 className="text-center text-indigo-800 text-3xl md:text-5xl">
+            His Tasks
+          </h1>
+          <div className="container mx-auto w-full tasks-center justify-center">
+            <ul className="flex flex-col bg-blue-300 p-4">
+              {hisTasks.map((task, index) => (
+                <li
+                  className={
+                    HisSelectedTask === index
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }
+                  onClick={() => {
+                    setHisSelectedTask(index);
+                    onSelectHisTask(task);
+                  }}
+                  key={index}
+                >
+                  <div className="select-none bg-gray-200 rounded-md flex flex-1 tasks-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg mb-4">
+                    <div className="flex flex-col rounded-md w-10 h-10 bg-gray-300 justify-center tasks-center mr-4">
+                      ⚽️
+                    </div>
+                    <div className="flex-1 pl-1 mr-16">
+                      <div className="font-medium">{task.description}</div>
+                      <div className="text-gray-600 text-sm">200ml</div>
+                    </div>
+                    <div className="text-gray-600 text-xs">
+                      6:00 AM
+                      <button
+                        className="text-red-500 ml-2"
+                        onClick={() => onDeleteTask(task.id!)}
+                      >
+                        delete task
+                      </button>
+                      <div>
+                        <label htmlFor="update-task">Update Task</label>
+                        <input
+                          id="update-task"
+                          className="text-indigo-500"
+                          onChange={(e) => setUpdatedTask(e.target.value)}
+                        />
+                        <button
+                          className="mr-2"
+                          onClick={() => onUpdateTask(task.id!, updatedTask)}
+                        >
+                          Update Task
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <Button />
+          </div>
+        </div>
+      )}
+
+      {/* haven't decided where to put this yet */}
+      {/* <div className="mx-auto w-2/5">
         <input
           className="mr-6"
           type="text"
@@ -38,102 +157,7 @@ function ListTable({
         />
         <input type="text" placeholder="Search Tasks By Date" />
       </div>
-      <h1 className="text-center">Her Tasks</h1>
-      <div className="container mx-auto w-full tasks-center justify-center">
-        <ul className="flex flex-col bg-pink-300 p-4">
-          {herTasks.map((task, index) => (
-            <li
-              className={
-                herSelectedTask === index ? "text-green-500" : "text-red-500"
-              }
-              onClick={() => {
-                setHerSelectedTask(index);
-                onSelectHerTask(task);
-              }}
-              key={index}
-            >
-              <div className="select-none bg-gray-200 rounded-md flex flex-1 tasks-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg mb-4">
-                <div className="flex flex-col rounded-md w-10 h-10 bg-gray-300 justify-center tasks-center mr-4">
-                  ⚽️
-                </div>
-                <div className="flex-1 pl-1 mr-16">
-                  <div className="font-medium">{task.description}</div>
-                  <div className="text-gray-600 text-sm">200ml</div>
-                </div>
-                <div className="text-gray-600 text-xs">
-                  6:00 AM
-                  <button
-                    className="text-red-500 ml-2"
-                    onClick={() => onDeleteTask(task.id!)}
-                  >
-                    delete task
-                  </button>
-                  <div>
-                    <label htmlFor="update-task">Update Task</label>
-                    <input
-                      id="update-task"
-                      className="text-indigo-500"
-                      onChange={(e) => setUpdatedTask(e.target.value)}
-                    />
-                    <button onClick={() => onUpdateTask(task.id!, updatedTask)}>
-                      Update Task
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <Button />
-      </div>
-      <h1 className="text-center">His Tasks</h1>
-      <div className="container mx-auto w-full tasks-center justify-center">
-        <ul className="flex flex-col bg-blue-300 p-4">
-          {hisTasks.map((task, index) => (
-            <li
-              className={
-                HisSelectedTask === index ? "text-green-500" : "text-red-500"
-              }
-              onClick={() => {
-                setHisSelectedTask(index);
-                onSelectHisTask(task);
-              }}
-              key={index}
-            >
-              <div className="select-none bg-gray-200 rounded-md flex flex-1 tasks-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg mb-4">
-                <div className="flex flex-col rounded-md w-10 h-10 bg-gray-300 justify-center tasks-center mr-4">
-                  ⚽️
-                </div>
-                <div className="flex-1 pl-1 mr-16">
-                  <div className="font-medium">{task.description}</div>
-                  <div className="text-gray-600 text-sm">200ml</div>
-                </div>
-                <div className="text-gray-600 text-xs">
-                  6:00 AM
-                  <button
-                    className="text-red-500 ml-2"
-                    onClick={() => onDeleteTask(task.id!)}
-                  >
-                    delete task
-                  </button>
-                  <div>
-                    <label htmlFor="update-task">Update Task</label>
-                    <input
-                      id="update-task"
-                      className="text-indigo-500"
-                      onChange={(e) => setUpdatedTask(e.target.value)}
-                    />
-                    <button className="mr-2" onClick={() => onUpdateTask(task.id!, updatedTask)}>
-                      Update Task
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <Button />
-      </div>
+     */}
     </>
   );
 }
